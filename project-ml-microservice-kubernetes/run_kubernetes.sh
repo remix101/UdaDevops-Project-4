c299f16f71f97f46afcbfa33a9559ee23f9e66d3
Ubuntu <ubuntu@ip-172-31-6-147.ec2.internal>
2022-08-01 23:59:10 +0000

    agentq docker

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="agentq/microserviceproject"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run microserviceproject --image=$dockerpath --port=80 --labels app=microserviceproject
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward microserviceproject 8000:80
